c6404626114363962bbf37ffd20e7da90cb40b18
Wilson Neto <contato@wilsonneto.com.br>
2021-12-31 12:36:30 -0300

    OwnedTypes

---
+++ OwnedTypes/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        using var db = new AppContext();
+    }
+}
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public Address Address { get; set; }
+}
+
+public class Address
+{
+    public int Number { get; set; }
+    public string Street { get; set; }
+    public string District { get; set; }
+}
+
+public class AppContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+
+    public AppContext() : base()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .UseSqlServer(@"Server=localhost;Database=company02;User Id=sa;Password=Str0ngP455W0RD");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(p => {
+            p.OwnsOne( x => x.Address, a => {
+                a.Property(p => p.District).HasColumnName("neighborhood");
+                a.ToTable("endereco");
+            });
+        });
+    }
+}
+
+/*
+    p.OwnsOne( x => x.Address ):
+
+    CREATE TABLE [Employees] (
+          [Id] int NOT NULL IDENTITY,
+          [Name] nvarchar(max) NOT NULL,
+          [SurName] nvarchar(max) NOT NULL,
+          [Address_Number] int NOT NULL,
+          [Address_Street] nvarchar(max) NOT NULL,
+          [Address_District] nvarchar(max) NOT NULL,
+          CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+      );
+
+    ------------------------------------------
+    p.OwnsOne( x => x.Address ).ToTable("endereco"):
+
+    CREATE TABLE [Employees] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [SurName] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+    );
+    CREATE TABLE [endereco] (
+        [EmployeeId] int NOT NULL,
+        [Number] int NOT NULL,
+        [Street] nvarchar(max) NOT NULL,
+        [District] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_endereco] PRIMARY KEY ([EmployeeId]),
+        CONSTRAINT [FK_endereco_Employees_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [Employees] ([Id]) ON DELETE CASCADE
+    );
+
+    -----------------------------------------
+    modelBuilder.Entity<Employee>(p => {
+        p.OwnsOne( x => x.Address, a => {
+            a.Property(p => p.District).HasColumnName("neighborhood");
+        });
+    });
+
+    CREATE TABLE [Employees] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [SurName] nvarchar(max) NOT NULL,
+        [Address_Number] int NOT NULL,
+        [Address_Street] nvarchar(max) NOT NULL,
+        [neighborhood] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+    );
+
+    ------------------------------------------------
+    modelBuilder.Entity<Employee>(p => {
+        p.OwnsOne( x => x.Address, a => {
+            a.Property(p => p.District).HasColumnName("neighborhood");
+            a.ToTable("endereco");
+        });
+    });
+
+    CREATE TABLE [Employees] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [SurName] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+    );
+
+    CREATE TABLE [endereco] (
+        [EmployeeId] int NOT NULL,
+        [Number] int NOT NULL,
+        [Street] nvarchar(max) NOT NULL,
+        [neighborhood] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_endereco] PRIMARY KEY ([EmployeeId]),
+        CONSTRAINT [FK_endereco_Employees_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [Employees] ([Id]) ON DELETE CASCADE
+    );
+*/
--- Readme.md
+++ Readme.md
@@ -1,3 +1,63 @@
 Para criação do banco de dados com docker:
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Str0ngP455W0RD" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-CU14-ubuntu-20.04
 
+### Shadow Properties
+
+```
+        // defining the shadow property
+        modelBuilder.Entity<Employee>().Property<DateTime>("LastUpdate"); 
+
+        // inserting the shadow property
+        db.Entry(Jorge).Property("LastUpdate").CurrentValue = DateTime.Now;
+
+        // searching by a shadow property
+        var result = db.Employees.Where(p => EF.Property<DateTime>(p, "LastUpdate") > DateTime.Now.AddHours(-1)).ToList();
+```
+
+
+### Owned Types
+
+```
+    modelBuilder.Entity<Employee>(p => {
+        p.OwnsOne( x => x.Address, a => {
+            a.Property(p => p.District).HasColumnName("neighborhood");
+        });
+    });
+
+    CREATE TABLE [Employees] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [SurName] nvarchar(max) NOT NULL,
+        [Address_Number] int NOT NULL,
+        [Address_Street] nvarchar(max) NOT NULL,
+        [neighborhood] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+    );
+
+```
+
+```
+    modelBuilder.Entity<Employee>(p => {
+        p.OwnsOne( x => x.Address, a => {
+            a.Property(p => p.District).HasColumnName("neighborhood");
+            a.ToTable("endereco");
+        });
+    });
+
+    CREATE TABLE [Employees] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [SurName] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+    );
+
+    CREATE TABLE [endereco] (
+        [EmployeeId] int NOT NULL,
+        [Number] int NOT NULL,
+        [Street] nvarchar(max) NOT NULL,
+        [neighborhood] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_endereco] PRIMARY KEY ([EmployeeId]),
+        CONSTRAINT [FK_endereco_Employees_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [Employees] ([Id]) ON DELETE CASCADE
+    );
+```
+
