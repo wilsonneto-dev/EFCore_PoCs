b341de2242e346070556f2ec5ff7e937aa36b230
Wilson Neto <contato@wilsonneto.com.br>
2022-01-04 21:18:20 -0300

    many to many with only Ids

--- ManyToManyWithOnlyIds/Program.cs
+++ ManyToManyWithOnlyIds/Program.cs
@@ -11,11 +11,11 @@ public class Program
         var repo = new MovieRepository(db);
         
         
-        /* Actor a = new Actor() { Name = "Keano Reeves", Id = Guid.NewGuid() };
+        Actor a = new Actor() { Name = "Keano Reeves", Id = Guid.NewGuid() };
         Actor a2 = new Actor() { Name = "Jenifer Lawrence", Id = Guid.NewGuid() };
         db.Actors.Add(a);
         db.Actors.Add(a2);
-        db.SaveChanges(); */
+        db.SaveChanges(); 
         
 
         
@@ -26,17 +26,21 @@ public class Program
             ActorsIds = new List<Guid>() { new Guid("88237949-b13e-4793-bd5a-1eb01397bd94") , new Guid("c684c39e-22e6-410a-9291-98019510c98c")}
         };
         
+        
+        var movie = repo.Get(new Guid("3e35b3a6-ccd9-4fcd-aa0c-5dc95a3d43a3"));
+        Console.WriteLine(movie.Name);
+        Console.WriteLine(movie.ActorsIds.Count());
 
-        repo.Insert(movie01);
+        movie.ActorsIds.Remove(movie.ActorsIds[1]);
+        repo.Update(movie);
 
-        /*
         var ma = new MovieActor() { 
             ActorId = new Guid("6575fe75-be17-4e7e-aa66-1ed9ae6e6b9b"), 
             MovieId = new Guid("b8762ed8-3b19-441c-a60c-88b913437b11") 
         };
         db.MovieActor.Add(ma);
         db.SaveChanges();
-        */
+
     }
 }
 
@@ -85,10 +89,18 @@ public class MovieRepository
 
     public void Update(Movie movie)
     {
-        /* _db.Movies.Update(movie);
-        _db.MovieActor.RemoveRange(_db.MovieActor.Where(x => ));
-        _db.MovieActor.AddRange(movie.ActorIds.Select(x => new MovieActor(){ ActorId = x, MovieId = movie.Id }));
-        _db.SaveChanges(); */
+        _db.Movies.Update(movie);
+        _db.MovieActor.RemoveRange(_db.MovieActor.Where(x => x.MovieId == movie.Id));
+        _db.MovieActor.AddRange(movie.ActorsIds.Select(x => new MovieActor(){ ActorId = x, MovieId = movie.Id }));
+        _db.SaveChanges();
+    }
+
+    public Movie? Get(Guid id)
+    {
+        var movie = _db.Movies.FirstOrDefault(x => x.Id == id);
+        if(movie == null) return null;
+        movie.ActorsIds = _db.MovieActor.Where(x => x.MovieId == movie.Id).Select(x => x.ActorId).ToList();
+        return movie;
     }
 }
 
