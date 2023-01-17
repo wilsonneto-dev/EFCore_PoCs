0e1a2ccd72a50a1ebdba8db0ba01b877c2555412
Wilson Neto <wilson.neto@dreamdev.io>
2023-01-17 20:44:17 +0000

    testing

--- OwnedTypes/Program.cs
+++ OwnedTypes/Program.cs
@@ -1,54 +1,94 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        using var db = new AppContext();
-        var employee = new Employee() { Id = Random.Shared.Next(10, 300), Address = null, Name = "Teste" };
-        db.Add(employee);
-        db.SaveChanges();
+        var id = Guid.Empty;
+        using (var db = new AppContext())
+        {
+            var video = new Video("Test", "ooook");
+            // video.UpdateMedia("old", "old");
+            // video.UpdateTrailer("old", "old");
+            // video.UpdateThumb("thumb-path");
+            // video.UpdateBanner("banner-path");
 
-        // employee.Address = new Address() { District = "Bairro", Number = 30, Street = "Avenue St." };
-        // db.Update(employee);
-        // db.SaveChanges();
+            id = video.Id;
+            db.Add(video);
+            Console.WriteLine(JsonSerializer.Serialize(video, new JsonSerializerOptions() { WriteIndented = true }));
+            // Console.WriteLine(JsonSerializer.Serialize(new{
+            //     video = db.Entry(video).State,
+            //     media = db.Entry(video.Media).State,
+            //     trailer = db.Entry(video.Trailer).State,
+            // }));
+            db.SaveChanges();
+        }
 
-        var db2 = new AppContext();
-        var employee2 = db2.Employees.AsNoTracking().Where(x => x.Id == employee.Id).First();
-        employee2.Address = new Address(){ Number = 10, District = "Bairro" };
-        db2.Update(employee2);
-        db2.SaveChanges();
+        Console.WriteLine(" --- updating...");
+        var newDbContext = new AppContext();
+        var video2 = newDbContext.Videos.AsNoTracking().Where(x => x.Id == id).First();
+
+        video2.UpdateThumb("new:thumb-path");
+        video2.UpdateBanner("new:banner-path");
+        video2.UpdateMedia("new:media-path", "new:encoded-media-path");
+        video2.UpdateTrailer("new:trailer-path", "new:encoded-trailer-path");
+
+        Console.WriteLine(JsonSerializer.Serialize(video2, new JsonSerializerOptions() { WriteIndented = true }));
+
+        // newDbContext.Add(video2);
+        newDbContext.Videos.Update(video2);
+
+        // Console.WriteLine(JsonSerializer.Serialize(new{
+        //     video = newDbContext.Entry(video2).State,
+        //     media = newDbContext.Entry(video2.Media).State,
+        //     trailer = newDbContext.Entry(video2.Trailer).State,
+        // }));
+
+        newDbContext.SaveChanges();
     }
-}
-
-public class Employee
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? SurName { get; set; }
-    public Address? Address { get; set; }
-}
-
-public class Address
-{
-    public int Number { get; set; }
-    public string? Street { get; set; }
-    public string? District { get; set; }
 }
 
 public class AppContext : DbContext
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Video>(p => {
+            p.Property(x => x.Id).ValueGeneratedNever();
+            p.Navigation(x => x.Media).AutoInclude();
+            p.Navigation(x => x.Trailer).AutoInclude();
+
+            p.OwnsOne( x => x.Thumb, a => {
+                a.Property(p => p.Path).IsRequired(false).HasColumnName("ThumbPath");
+            });
+
+            p.OwnsOne( x => x.Banner, a => {
+                a.Property(p => p.Path).IsRequired(false).HasColumnName("BannerPath");
+            });
+            
+            p.HasOne(x => x.Media).WithOne().HasForeignKey<Video>("MediaId");
+            p.HasOne(x => x.Trailer).WithOne().HasForeignKey<Video>("TrailerId");
+            // p.HasOne(x => x.Media).WithOne("VideoId").HasForeignKey<Media>();
+        });
+
+        modelBuilder.Entity<Media>(p => {
+            // p.HasKey(x => x.Id);
+            p.Property(x => x.Id).ValueGeneratedNever();
+        });
+    }
+
     static bool Created = false;
 
-    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Video> Videos => Set<Video>();
 
     public AppContext() : base()
     {
         if(Created == true)
             return;
 
-        // Database.EnsureDeleted();
+        Database.EnsureDeleted();
         Database.EnsureCreated();
         Created = true;
     }
@@ -58,94 +98,77 @@ public class AppContext : DbContext
         optionsBuilder
             .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
             .EnableSensitiveDataLogging()
+            .UseSqlServer(@"Server=localhost;Database=company02;User Id=sa;Password=Str0ngP455W0RD;trustServerCertificate=true;");
             //.UseSqlite("Data Source=./Application.db;");
-            .UseSqlite("Data Source=Sharable;Mode=Memory;Cache=Shared");
+            // .UseSqlite("Data Source=Sharable;Mode=Memory;Cache=Shared");
             // .UseInMemoryDatabase("general");
-            // .UseSqlServer(@"Server=localhost;Database=company02;User Id=sa;Password=Str0ngP455W0RD;trustServerCertificate=true;");
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Employee>(p => {
-            p.OwnsOne( x => x.Address, a => {
-                a.Property(p => p.District).HasColumnName("neighborhood");
-                // a.ToTable("endereco");
-            });
-        });
-
-        modelBuilder.Entity<Employee>().Property(x => x.Id).ValueGeneratedNever();
     }
 }
 
-/*
-    p.OwnsOne( x => x.Address ):
+public class Video
+{
+    public Video(string title, string description)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+    }
 
-    CREATE TABLE [Employees] (
-          [Id] int NOT NULL IDENTITY,
-          [Name] nvarchar(max) NOT NULL,
-          [SurName] nvarchar(max) NOT NULL,
-          [Address_Number] int NOT NULL,
-          [Address_Street] nvarchar(max) NOT NULL,
-          [Address_District] nvarchar(max) NOT NULL,
-          CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
-      );
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    
+    public Media? Media { get; private set; }
+    public Media? Trailer { get; private set; }
+    public Image? Thumb { get; private set; }
+    public Image? Banner { get; private set; }
 
-    ------------------------------------------
-    p.OwnsOne( x => x.Address ).ToTable("endereco"):
+    public void UpdateThumb(string path) => Thumb = new Image(path);
+    
+    public void UpdateBanner(string path) => Banner = new Image(path);
 
-    CREATE TABLE [Employees] (
-        [Id] int NOT NULL IDENTITY,
-        [Name] nvarchar(max) NOT NULL,
-        [SurName] nvarchar(max) NOT NULL,
-        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
-    );
-    CREATE TABLE [endereco] (
-        [EmployeeId] int NOT NULL,
-        [Number] int NOT NULL,
-        [Street] nvarchar(max) NOT NULL,
-        [District] nvarchar(max) NOT NULL,
-        CONSTRAINT [PK_endereco] PRIMARY KEY ([EmployeeId]),
-        CONSTRAINT [FK_endereco_Employees_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [Employees] ([Id]) ON DELETE CASCADE
-    );
+    public void UpdateMedia(string path, string encodedPath)
+    {
+        if(Media is null)
+            Media = new Media(path, encodedPath);
+        else
+            Media.Update(path, encodedPath);
+    }
 
-    -----------------------------------------
-    modelBuilder.Entity<Employee>(p => {
-        p.OwnsOne( x => x.Address, a => {
-            a.Property(p => p.District).HasColumnName("neighborhood");
-        });
-    });
+    public void UpdateTrailer(string path, string encodedPath)
+    {
+        if(Trailer is null)
+            Trailer = new Media(path, encodedPath);
+        else
+            Trailer.Update(path, encodedPath);
+    }
+}
 
-    CREATE TABLE [Employees] (
-        [Id] int NOT NULL IDENTITY,
-        [Name] nvarchar(max) NOT NULL,
-        [SurName] nvarchar(max) NOT NULL,
-        [Address_Number] int NOT NULL,
-        [Address_Street] nvarchar(max) NOT NULL,
-        [neighborhood] nvarchar(max) NOT NULL,
-        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
-    );
+public class Image
+{
+    public Image(string path) => Path = path;
 
-    ------------------------------------------------
-    modelBuilder.Entity<Employee>(p => {
-        p.OwnsOne( x => x.Address, a => {
-            a.Property(p => p.District).HasColumnName("neighborhood");
-            a.ToTable("endereco");
-        });
-    });
+    public string Path { get; init; }
+}
 
-    CREATE TABLE [Employees] (
-        [Id] int NOT NULL IDENTITY,
-        [Name] nvarchar(max) NOT NULL,
-        [SurName] nvarchar(max) NOT NULL,
-        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
-    );
+public class Media
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public Guid Id { get; private set; }
+    public string Path { get; private set; }
+    public string EncodedPath { get; private set; }
 
-    CREATE TABLE [endereco] (
-        [EmployeeId] int NOT NULL,
-        [Number] int NOT NULL,
-        [Street] nvarchar(max) NOT NULL,
-        [neighborhood] nvarchar(max) NOT NULL,
-        CONSTRAINT [PK_endereco] PRIMARY KEY ([EmployeeId]),
-        CONSTRAINT [FK_endereco_Employees_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [Employees] ([Id]) ON DELETE CASCADE
-    );
-*/
+    public Media(string path, string encodedPath)
+    {
+        Id = Guid.NewGuid();
+        Path = path;
+        EncodedPath = encodedPath;
+    }
+
+    public void Update(string path, string encodedPath)
+    {
+        Path = path;
+        EncodedPath = encodedPath;
+    }
+}
